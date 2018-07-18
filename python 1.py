8e9f9020b203f6faac02d8946d200a72d7e80845
godstaxz <41391638+godstaxz@users.noreply.github.com>
2018-07-18 14:20:43 -0500

    Add files via upload

---
+++ python 1.py
@@ -0,0 +1,32 @@
+#basic python stuff
+print('We\'re back at it man, python lives forever #NeverDie +++++++')
+
+print('Hi' + ' there')
+print('8.5 + 5 =', 8.5 + 5)
+
+
+exampleVar = print('whoa chill')
+print(exampleVar)
+
+x,y = (3,5)
+print(x)
+print(y)
+
+#While loops
+condition = 1
+while condition < 10:
+    print(condition)
+    condition +=1
+
+
+while True:
+    print('doing stuff')   
+
+#For Loop
+exampleList = [1,2,3,4,5,6,7,8,9,1345]
+
+for eachNumber in exampleList:
+    print(eachNumber)
+
+for x in range(1,5):
+    print(x)
